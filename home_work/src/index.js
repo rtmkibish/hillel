9c826ed1aa8f1c35c2170d02a0ddbaf12dcc31b9
artem.kibish <artem.kibish@okta.com>
2020-07-15 21:21:09 +0300

    Home work 8. The second attempt

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,8 +1,8 @@
 function map(array, callback) {
   const resultArray = [];
 
-  for(let item of array) {
-    const result = callback(item);
+  for(let i = 0; i < array.length; i++) {
+    const result = callback(array[i], i, array);
     resultArray.push(result);
   }
 
@@ -12,26 +12,24 @@ function map(array, callback) {
 function filter(array, callback) {
   const resultArray = [];
 
-  for(let prop of array) {
-    const condition = callback(prop);
+  for(let i = 0; i < array.length; i++) {
+    const condition = callback(array[i], i, array);
 
-    if(condition) resultArray.push(prop);
+    if(condition) resultArray.push(array[i]);
   }
 
   return resultArray;
 }
 
 function getMessagesByDate(notifacations) {
-  const messagesByDate = {}
-
-  for(let prop of notifacations) {
-    if(messagesByDate.hasOwnProperty(prop.date)) {
-      messagesByDate[prop.date].push(prop.msg);
-    } else {
-      messagesByDate[prop.date] = [];
-      messagesByDate[prop.date].push(prop.msg);
+  const messagesByDate = notifacations.reduce((prev, item, index, arr) => {
+    if(!prev[item.date]) {
+      prev[item.date] = [];
     }
-  }
+    prev[item.date].push(item.msg);
+
+    return prev;
+  }, {});
 
   return messagesByDate;
 }
