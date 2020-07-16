6fc63c63181918059f144c53834d1aa81d3cce74
artem.kibish <artem.kibish@okta.com>
2020-07-16 18:54:44 +0300

    Raw HW 9 added

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,37 +1,63 @@
-function map(array, callback) {
-  const resultArray = [];
+function ToDoList() {
 
-  for(let i = 0; i < array.length; i++) {
-    const result = callback(array[i], i, array);
-    resultArray.push(result);
-  }
-
-  return resultArray;
-}
-
-function filter(array, callback) {
-  const resultArray = [];
-
-  for(let i = 0; i < array.length; i++) {
-    const condition = callback(array[i], i, array);
-
-    if(condition) resultArray.push(array[i]);
-  }
-
-  return resultArray;
-}
-
-function getMessagesByDate(notifacations) {
-  const messagesByDate = notifacations.reduce((prev, item, index, arr) => {
-    if(!prev[item.date]) {
-      prev[item.date] = [];
+Object.defineProperties(this, {
+  _tasks: {
+    value: [],
+  },
+  tasks: {
+    get() {
+      return this._tasks.slice();
     }
-    prev[item.date].push(item.msg);
+  },
+  addTask: {
+    value: function(task, confirmed) {
+      if (confirmed) {
+        const isDuplicate = this._tasks.find(t => {
+          return t.title === task.title && t.text === task.text && t.isDone === task.isDone;
+        });
+        console.log(isDuplicate);
+        if (!isDuplicate) {
+          task.id = Date.now() + (Math.floor(Math.random() * 1000));
+          task.created = new Date().toISOString();
+          return this._tasks.push(task);
+        }
+      }
+    }
+  },
+  deleteTask: {
+    value: function(id, confirmed) {
+      if (confirmed) {
+        const taskToDeleteIndex = this._tasks.findIndex(item => item.id === id);
+        if (taskToDeleteIndex >= 0) {
+          this._tasks.splice(taskToDeleteIndex, 1);
+        }
+      }
+    }
+  },
+  editTask: {
+    value: function(task, confirmed) {
+      if (confirmed) {
+        const taskToChange = this._tasks.find(t => t.id === task.id);
+        Object.assign(taskToChange, task);
+      }
+    }
+  },
+  getDone: {
+    value: function() {
+      const doneTasks = this._tasks.filter(task => task.isDone === true);
+      return doneTasks;
+    },
+  },
+  getNotDone: {
+    value: function() {
+      const notDoneTasks = this._tasks.filter(task => task.isDone === false);
+      return notDoneTasks;
+    },
+  },
+});
 
-    return prev;
-  }, {});
+Object.preventExtensions(this);
 
-  return messagesByDate;
 }
 
-module.exports = { map, filter, getMessagesByDate };
+const toDoList = new ToDoList();
