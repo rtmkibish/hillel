aba620bb26858b6f01560ff848133f0357f8e67a
artem.kibish <artem.kibish@okta.com>
2020-07-18 21:27:30 +0300

    Home work 9 added

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -10,48 +10,56 @@ Object.defineProperties(this, {
     }
   },
   addTask: {
-    value: function(task, confirmed) {
-      if (confirmed) {
-        const isDuplicate = this._tasks.find(t => {
-          return t.title === task.title && t.text === task.text && t.isDone === task.isDone;
-        });
-        console.log(isDuplicate);
-        if (!isDuplicate) {
-          task.id = Date.now() + (Math.floor(Math.random() * 1000));
-          task.created = new Date().toISOString();
-          return this._tasks.push(task);
+    value: function(data, confirmed) {
+      if (confirmed && !this._isDuplicate(data)) {
+          data.id = Date.now() + (Math.floor(Math.random() * 1000));
+          data.createdAt = new Date().toISOString();
+          const newTaskIndex = this._tasks.push(data) - 1;
+          return this._tasks[newTaskIndex];
         }
       }
-    }
   },
   deleteTask: {
     value: function(id, confirmed) {
       if (confirmed) {
         const taskToDeleteIndex = this._tasks.findIndex(item => item.id === id);
         if (taskToDeleteIndex >= 0) {
-          this._tasks.splice(taskToDeleteIndex, 1);
+          return this._tasks.splice(taskToDeleteIndex, 1);
         }
       }
     }
   },
   editTask: {
-    value: function(task, confirmed) {
-      if (confirmed) {
+    value: function(task, data, confirmed) {
+      if (confirmed && !this._isDuplicate(data)) {
         const taskToChange = this._tasks.find(t => t.id === task.id);
-        Object.assign(taskToChange, task);
+        return Object.assign(taskToChange, data);
       }
     }
   },
-  getDone: {
+  getNumberOfDoneTasks: {
     value: function() {
       const doneTasks = this._tasks.filter(task => task.isDone === true);
-      return doneTasks;
+      return doneTasks.length;
     },
   },
-  getNotDone: {
+  getNumberOfNotDoneTasks: {
     value: function() {
       const notDoneTasks = this._tasks.filter(task => task.isDone === false);
-      return notDoneTasks;
+      return notDoneTasks.length;
+    },
+  },
+  getNumberOfAllTasks: {
+    value: function() {
+      return this._tasks.length;
+    }
+  },
+  _isDuplicate: {
+    value: function(task) {
+      const isDuplicate = this._tasks.find(t => {
+      return t.title === task.title && t.text === task.text && t.isDone === task.isDone;
+      });
+      return isDuplicate;
     },
   },
 });
@@ -60,4 +68,4 @@ Object.preventExtensions(this);
 
 }
 
-const toDoList = new ToDoList();
+module.exports = { ToDoList };
--- home_work/src/index.spec.js
+++ home_work/src/index.spec.js
@@ -1,51 +1,152 @@
-const { map, filter, getMessagesByDate } = require("./index");
+const { ToDoList } = require("./index");
 
-describe("The map test", () => {
+describe("The ToDoList function tests", () => {
 
-    test("1: ([1,2,3,4], c) => [1, 4, 9, 16]", () => {
-        const pow = function(el) {
-            return el ** 2;
-        }
-        expect(map([1,2,3,4], pow)).toEqual([1, 4, 9, 16]);
+  test("1: To be unextended", () => {
+    const toDoList = new ToDoList();
+    toDoList.newProp = 1;
+    expect(toDoList).toEqual(new ToDoList());
+  });
+
+  test("2: To be immutable", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask = 1;
+    expect(toDoList).toEqual(new ToDoList());
+  });
+
+  test("3: Can't delete a property", () => {
+    const toDoList = new ToDoList();
+    delete toDoList.deleteTask;
+    expect(toDoList).toEqual(new ToDoList());
+  });
+
+  test("4: Can't rewrite the ToDoList()._tasks", () => {
+    const toDoList = new ToDoList();
+    toDoList._tasks = [{a: 1}];
+    expect(toDoList).toEqual(new ToDoList());
+  });
+
+  test("5: Can't rewrite the ToDoList().tasks getter", () => {
+    const toDoList = new ToDoList();
+    toDoList.tasks = [{a: 1}];
+    expect(toDoList).toEqual(new ToDoList());
+  });
+
+  test("6: Can't reconfigure a property", () => {
+    const toDoList = new ToDoList();
+    expect( () => {
+      Object.defineProperty(toDoList, "_tasks", {writable: true})
+    }).toThrow("Cannot redefine property: _tasks");
+  });
+
+  test("7: Add a task", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    }, true);
+    expect(toDoList.tasks.length).toBe(1);
+  });
+
+  test("8: Can't add a duplicate task", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    }, true);
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    }, true);    
+    expect(toDoList.tasks.length).toBe(1);
+  });
+
+  test("9: Edit a task", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    }, true);
+    const taskToEdit = toDoList.tasks[0];
+    taskToEdit.title = "Buy water";
+    taskToEdit.text = "Buy water because it is hot"
+    toDoList.editTask(taskToEdit, true);
+    expect(toDoList.tasks[0]).toEqual({
+      title: "Buy water",
+      text: "Buy water because it is hot",
+      isDone: false,
+      createdAt: taskToEdit.createdAt,
+      id: taskToEdit.id
     });
-});
+  });
 
-describe("The filter test", () => {
-
-    test("2: ([1,2,3,4], c) => [2, 4]", () => {
-        const even = function(el) {
-            return el % 2 === 0;
-        }
-        expect(filter([1,2,3,4], even)).toEqual([2, 4]);
+  test("10: Can't edit to duplicate the task", () => {
+    const toDoList = new ToDoList();
+    const buyMilk = {
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    };
+    const buySalt = {
+      title: "Buy salt",
+      text: "Buy salt for a salad",
+      isDone: false,};
+    const buyMilkTask = toDoList.addTask(buyMilk, true);
+    const buySaltTask = toDoList.addTask(buySalt, true);
+    toDoList.editTask(buyMilkTask, buySalt, true);
+    expect(toDoList.tasks[0]).toEqual({
+      title: buyMilkTask.title,
+      text: buyMilkTask.text,
+      isDone: buyMilkTask.isDone,
+      id: buyMilkTask.id,
+      createdAt: buyMilkTask.createdAt,
     });
-});
+  });
 
-describe("The getMessagesByDate test", () => {
+  test("11: Delete a task", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    }, true);
+    const firstTask = toDoList.tasks[0];
+    const tasksNumberBeforeTest = toDoList.tasks.length;
+    toDoList.deleteTask(firstTask.id, true);
+    expect(toDoList.tasks.length).not.toBe(tasksNumberBeforeTest);
+  });
 
-    test("3: ([{date: '31/07/2019', msg: 'text', ...}] => {'31/07/2019': ['text']})", () => {
-        const messages = [
-            {
-                date: "31/07/20",
-                msg: "msg1"
-            },
-            {
-                date: "02/07/20",
-                msg: "msg2"
-            },
-            {
-                date: "31/07/20",
-                msg: "msg3"
-            },
-            {
-                date: "01/07/20",
-                msg: "msg4"
-            }
-        ];
-        expect(getMessagesByDate(messages))
-        .toEqual({
-            "31/07/20": ["msg1", "msg3"],
-            "02/07/20": ["msg2"],
-            "01/07/20": ["msg4"],
-        });
-    });
+  test("12: Get number of All tasks", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    }, true);
+    expect(toDoList.getNumberOfAllTasks()).toBe(1);
+  });
+
+  test("13: Get number of Done tasks", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: true,
+    }, true);
+    expect(toDoList.getNumberOfDoneTasks()).toBe(1);
+  });
+
+  test("14: Get number of Not Done tasks", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask({
+      title: "Buy milk",
+      text: "Buy milk to make a cake",
+      isDone: false,
+    }, true);
+    expect(toDoList.getNumberOfNotDoneTasks()).toBe(1);
+  });
 });
