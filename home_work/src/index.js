0d0152cee216518570f7feaedb47d343b3b01dce
artem.kibish <artem.kibish@okta.com>
2020-08-08 14:25:21 +0300

    Home work 13 added

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,71 +1,84 @@
 function ToDoList() {
 
-Object.defineProperties(this, {
-  _tasks: {
+  Object.defineProperty(this, "_tasks", {
     value: [],
-  },
-  tasks: {
-    get() {
-      return this._tasks.slice();
-    }
-  },
-  addTask: {
-    value: function(data, confirmed) {
-      if (confirmed && !this._isDuplicate(data)) {
-          data.id = Date.now() + (Math.floor(Math.random() * 1000));
-          data.createdAt = new Date().toISOString();
-          const newTaskIndex = this._tasks.push(data) - 1;
-          return this._tasks[newTaskIndex];
-        }
-      }
-  },
-  deleteTask: {
-    value: function(id, confirmed) {
-      if (confirmed) {
-        const taskToDeleteIndex = this._tasks.findIndex(item => item.id === id);
-        if (taskToDeleteIndex >= 0) {
-          return this._tasks.splice(taskToDeleteIndex, 1);
-        }
-      }
-    }
-  },
-  editTask: {
-    value: function(task, data, confirmed) {
-      if (confirmed && !this._isDuplicate(data)) {
-        const taskToChange = this._tasks.find(t => t.id === task.id);
-        return Object.assign(taskToChange, data);
-      }
-    }
-  },
-  getNumberOfDoneTasks: {
-    value: function() {
-      const doneTasks = this._tasks.filter(task => task.isDone === true);
-      return doneTasks.length;
-    },
-  },
-  getNumberOfNotDoneTasks: {
-    value: function() {
-      const notDoneTasks = this._tasks.filter(task => task.isDone === false);
-      return notDoneTasks.length;
-    },
-  },
-  getNumberOfAllTasks: {
-    value: function() {
-      return this._tasks.length;
-    }
-  },
+    writable: true,
+  });
+  Object.preventExtensions(this);
+}
+
+// Internal help method
+Object.defineProperties(ToDoList, {
   _isDuplicate: {
-    value: function(task) {
-      const isDuplicate = this._tasks.find(t => {
-      return t.title === task.title && t.text === task.text && t.isDone === task.isDone;
+    value: function (tasksList, task) {
+      const isDuplicate = tasksList.find(t => {
+        return t.title === task.title && t.text === task.text && t.isDone === task.isDone;
       });
       return isDuplicate;
     },
   },
+  _getTask: {
+    value: function ({
+      id
+    }, tasksList) {
+      const newTaskIndex = tasksList.findIndex(t => t.id === id);
+      return tasksList[newTaskIndex];
+    }
+  },
 });
 
-Object.preventExtensions(this);
+// Prototype for ToDoList
+Object.defineProperties(ToDoList.prototype, {
+  addTask: {
+    value: function (data, confirmed) {
+      if (confirmed && !ToDoList._isDuplicate(data, this._tasks)) {
+        data.id = Date.now() + (Math.floor(Math.random() * 1000));
+        data.createdAt = new Date().toISOString();
+        this._tasks = [data, ...this._tasks];
+        return ToDoList._getTask(data);
+      }
+    }
+  },
+  deleteTask: {
+    value: function (task, confirmed) {
+      if (confirmed) {
+        const deletedTask = ToDoList._getTask(task, this._tasks);
+        this._tasks = this._tasks.filter(t => t.id !== task.id);
+        return deletedTask;
+      }
+    }
+  },
+  editTask: {
+    value: function (task, data, confirmed) {
+      if (confirmed && !ToDoList._isDuplicate(data, this._tasks)) {
+        this._tasks = this._tasks.map(t => {
+          if (t.id === task.id) {
+            return {
+              ...t,
+              ...data
+            }
+          } else {
+            return t;
+          }
+        });
+      }
+      return ToDoList._getTask(task, this._tasks);
+    }
+  },
+  statistic: {
+    get: function () {
+      const statObj = this._tasks.reduce((prev, t) => ({
+        ...prev,
+        done: t.isDone ? ++prev.done : prev.done,
+      }), {
+        total: this._tasks.length,
+        done: 0
+      });
+      return statObj;
+    }
+  }
+});
 
-}
-
-module.exports = { ToDoList };
+module.exports = {
+  ToDoList
+};
